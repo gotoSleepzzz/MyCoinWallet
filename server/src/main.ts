4efc3c1800f5515daf78c1f64d5a1c1c023b3b9b
trang nguyen <az9thongnguyen@gmail.com>
2023-07-16 09:59:29 +0700

    [server]: update create + access wallet

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import { create } from 'domain';
+import { createWallet, createWalletUsingPassword, getWallet, getWalletFromPassword } from './models/wallet';
+import { get } from 'http';
 require('dotenv').config();
 
 const hostPort: number = parseInt(process.env.PORT as string, 10) || 8080;
@@ -12,9 +15,56 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.get('/', (req, res) => {
-    res.status(200).json({ message: 'Connected to server!' });
+  res.status(200).json({ message: 'Welcome to server!' });
+});
+
+app.post('/api/v1/createWallet', (req, res) => {
+  try {
+    const method = req.body.method;
+
+    if (method === 'usingPassword') {
+      const password = req.body.password;
+      if (password === undefined || password.length < 8) {
+        throw new Error('Invalid password');
+      }
+      const data = createWalletUsingPassword(password);
+      res.status(200).json(data);
+    }
+    else if (method === 'usingMnemonic') {
+      throw new Error('Unsupported method');
+    }
+    else if (method === 'usingPrivateKey') {
+      const data = createWallet();
+      res.status(200).json(data);
+    }
+    else {
+      throw new Error('Invalid method');
+    }
+  } catch (e) {
+    console.log(e);
+    res.status(400).send(e);
+  }
+});
+
+app.post('/api/v1/accessWallet', (req, res) => {
+  try {
+    const method = req.body.method;
+
+    if (method === 'usingPassword') {
+      const wallet = getWalletFromPassword(req.body.password, req.body.data);
+    } else if (method === 'usingMnemonic') {
+      throw new Error('Unsupported method');
+    } else if (method === 'usingPrivateKey') {
+      const wallet = getWallet(req.body.privateKey);
+    } else {
+      throw new Error('Invalid method');
+    }
+  } catch (e) {
+    console.log(e);
+    res.status(400).send(e);
+  }
 });
 
 app.listen(hostPort, () => {
-    console.log('Server is listening on port ' + hostPort);
+  console.log('Server is listening on port ' + hostPort);
 });
--- server/src/models/wallet.ts
+++ server/src/models/wallet.ts
@@ -1,17 +1,86 @@
-import * as CryptoJS from 'crypto-js';
-import { v4 as uuidv4 } from 'uuid';
+import { ec } from "elliptic";
+import * as CryptoJS from "crypto-js";
 
+const EC = new ec('secp256k1');
 class Wallet {
-    privateKey: string;
-    address: string;
-    balance: number;
+    public privateKey: string;
+    public address: string;
+    public balance: number;
 
-    constructor(privateKey: string, address: string) { }
+    constructor() {
+        this.balance = 0;
+    }
+
+    createNew() {
+        const keyPair = this.generateKeyPair();
+        this.privateKey = `0x${keyPair.privateKey}`;
+        this.address = `0x${keyPair.publicKey}`;
+    }
 
     generateKeyPair(): any {
-        const privateKey = uuidv4().replace(/-/g, '');
-        const publicKey = CryptoJS.SHA256(privateKey).toString();
+        const keyPair = EC.genKeyPair();
+        const privateKey = keyPair.getPrivate('hex');
+        const publicKey = keyPair.getPublic('hex');
+        return { publicKey, privateKey };
+    }
 
-        return { privateKey, publicKey };
+    load(privateKey: string): boolean {
+        const keyPair = EC.keyFromPrivate(privateKey.substring(2, privateKey.length), 'hex');
+        this.address = `0x${keyPair.getPublic('hex')}`;
+        this.privateKey = `0x${keyPair.getPrivate('hex')}`;
+        return true;
+    }
+
+    loadFromPassword(password: string, address: string, passhash: string, crypted: string): boolean {
+        if (passhash === CryptoJS.SHA256(password).toString()) {
+            const privateKey = decryptPK(crypted, password);
+            const keyPair = EC.keyFromPrivate(privateKey, 'hex');
+            this.address = `0x${keyPair.getPublic('hex')}`;
+            this.privateKey = `0x${keyPair.getPrivate('hex')}`;
+        } else {
+            return false;
+        }
+        return this.address === address;
     }
 }
+
+const getPublickey = (privateKey: string): string => {
+    return `0x${EC.keyFromPrivate(privateKey, 'hex').getPublic('hex')}`;
+}
+
+const encryptPK = (privateKey: string, password: string): string => {
+    return CryptoJS.AES.encrypt(privateKey, password).toString();
+}
+const decryptPK = (crypted: string, password: string): string => {
+    return CryptoJS.AES.decrypt(crypted, password).toString(CryptoJS.enc.Utf8);
+}
+
+const createWalletUsingPassword = (password: string) => {
+    const wallet = new Wallet();
+    wallet.createNew();
+    const data = {
+        address: wallet.address,
+        passhash: CryptoJS.SHA256(password).toString(),
+        crypted: encryptPK(wallet.privateKey, password)
+    }
+    return data;
+}
+
+const createWallet = (): Wallet => {
+    return new Wallet();
+}
+
+const getWalletFromPassword = (password: string, data: string): Wallet => {
+    const dataJS = JSON.parse(data);
+    const wallet = new Wallet();
+    if (!wallet.loadFromPassword(password, dataJS.address, dataJS.passhash, dataJS.crypted)) throw new Error('Invalid password');
+    return wallet;
+}
+
+const getWallet = (privateKey: string): Wallet => {
+    const wallet = new Wallet();
+    if (!wallet.load(privateKey)) throw new Error('Invalid private key');
+    return wallet;
+}
+
+export { Wallet, createWalletUsingPassword, createWallet, getWalletFromPassword, getWallet };
